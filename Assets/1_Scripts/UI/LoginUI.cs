e7d844a34243d007acf69acdd5563f0b4eb02ff0
Jacudibu <jacudibu@gmail.com>
2021-03-20 23:24:49 +0100

    Login UI can now login

--- Assets/1_Scripts/UI/LoginUI.cs
+++ Assets/1_Scripts/UI/LoginUI.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -8,9 +9,41 @@ namespace UI
         [SerializeField] private TMP_InputField nameInput;
         [SerializeField] private TMP_InputField passwordInput;
 
+        [SerializeField] private GameObject inputParent;
+        [SerializeField] private GameObject successParent;
+
+        private const string CredentialName = "bla";
+        private const string CredentialPassword = "blub";
+        
         public void Login()
         {
             Debug.Log("Logging in with " + nameInput.text + " | " + passwordInput.text);
+
+            if (!nameInput.text.ToLower().Equals(CredentialName.ToLower()))
+            {
+                // TODO: Display account not found
+                Debug.Log("Account not found!");
+                return;
+            }
+            
+            if (!passwordInput.text.ToLower().Equals(CredentialPassword.ToLower()))
+            {
+                // TODO: Display password does not match
+                Debug.Log("Password does not match!");
+                return;
+            }
+
+            inputParent.SetActive(false);
+            successParent.SetActive(true);
+        }
+
+        private void OnEnable()
+        {
+            nameInput.text = "";
+            passwordInput.text = "";
+            
+            inputParent.SetActive(true);
+            successParent.SetActive(false);
         }
     }
 }
