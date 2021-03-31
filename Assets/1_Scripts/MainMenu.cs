32f3bb0c77470acc5d5bba4b2807e402c7e7bc6c
Uthak <f.mundschenk@web.de>
2021-03-31 10:34:59 +0200

    phone vibrate mainmenu script start UI hirarchy Logos import

--- Assets/1_Scripts/MainMenu.cs
+++ Assets/1_Scripts/MainMenu.cs
@@ -5,9 +5,48 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private SceneAsset firstLevel;
-    
+    [SerializeField] private GameObject localizationButtons;
+    [SerializeField] private GameObject organizerLogos;
+    [SerializeField] private GameObject mainInterface;
+    [SerializeField] private GameObject creditsScreen;
+
+
+
     public void OnPlayButtonPressed()
     {
         SceneManager.LoadScene(firstLevel.name);
+    }
+    
+    
+    public void OnLanguageSelected()
+    {
+        Debug.Log("You selected a language and can now access the main menu");
+        localizationButtons.SetActive(false);
+        organizerLogos.SetActive(false);
+        mainInterface.SetActive(true);
+    }
+    public void OnCreditsSelected()
+    {
+        Debug.Log("Credits Opened");
+        mainInterface.SetActive(false);
+        creditsScreen.SetActive(true);
+    }
+    public void OnBackSelected()
+    {
+        Debug.Log("Credits Closed");
+        creditsScreen.SetActive(false);
+        mainInterface.SetActive(true);
+    }
+    public void OnChangeLanguageSelected()
+    {
+        Debug.Log("Back to Languages (inverted languages in localization)");
+        mainInterface.SetActive(false);
+        organizerLogos.SetActive(true);
+        localizationButtons.SetActive(true);
+    }
+    public void OnQuitPressed()
+    {
+        Debug.Log("You pressed Quit!");
+        Application.Quit();
     }
 }
