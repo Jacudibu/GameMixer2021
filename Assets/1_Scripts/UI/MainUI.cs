a85933caaed2c69ad36503562826b59ee5a4b291
Jacudibu <jacudibu@gmail.com>
2021-04-16 10:42:47 +0200

    Reworked the about me section

--- Assets/1_Scripts/UI/MainUI.cs
+++ Assets/1_Scripts/UI/MainUI.cs
@@ -13,16 +13,26 @@ namespace UI
         [SerializeField] private TextMeshProUGUI nameText;
 
         [SerializeField] private Image profileImage;
+        [SerializeField] private TextMeshProUGUI aboutMeHeaderText;
+        [SerializeField] private TextMeshProUGUI dateOfBirthHeaderText;
         [SerializeField] private TextMeshProUGUI dateOfBirthText;
+        [SerializeField] private TextMeshProUGUI locationHeaderText;
         [SerializeField] private TextMeshProUGUI locationText;
+        [SerializeField] private TextMeshProUGUI mailText;
         [SerializeField] private TextMeshProUGUI hobbiesText;
 
         public void Initialize([NotNull] CharacterObject character)
         {
             profileImage.sprite = character.profilePicture;
             nameText.text = character.GetNameString();
+
+            aboutMeHeaderText.text = Localization.Localization.Get("aboutMe.Title");
+            dateOfBirthHeaderText.text = "<sprite=0>" + Localization.Localization.Get("aboutMe.Birthday");
+            locationHeaderText.text = "<sprite=0>" + Localization.Localization.Get("aboutMe.Location");
+            
             dateOfBirthText.text = character.dayOfBirth + " / " + character.monthOfBirth + " / " + character.yearOfBirth;
             locationText.text = Localization.Localization.Get(character.location);
+            mailText.text = Localization.Localization.Get(character.email);
             hobbiesText.text = string.Join("\n", character.hobbies.Select(Localization.Localization.Get));
         }
 
